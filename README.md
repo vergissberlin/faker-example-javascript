ae22c6b87b1b2a3cc4427b3ed0b21e698f97a191
Andre Lademann <andre.lademann@netresearch.de>
2021-06-17 09:38:40 +0200

    Add README file

---
+++ README.md
@@ -0,0 +1,26 @@
+# Multi thread fake data generater example with javascript
+
+## Using
+
+### Without Docker
+
+To use this method, you need to install nodejs >= 12 on your local system firtst.
+
+```bash
+yarn
+yarn start
+```
+
+### With Docker
+
+To use this method, you need to install Docker on your local system firtst.
+
+```bash
+docker run -it -v $PWD:/app -w /app node yarn
+docker run -it -v $PWD:/app -w /app node yarn start
+```
+
+## Wanna know more about?
+
+I wrote an article on medium about my journy in this topic.
+Go ahead! https://medium.com/p/98b990967824
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const FS = require('fs')
 const Path = require('path')
 const OS = require('os')
 const CLIProgress = require('cli-progress')
-const { Transform } = require('json2csv')
+const {Transform} = require('json2csv')
 
-const TARGET_PER_WORKER = 10
+const TARGET_PER_WORKER = 60000
 const INSTANCES = OS.cpus().length
 const DATASET_SIZE = INSTANCES * TARGET_PER_WORKER
 
 if (Cluster.isMaster) {
-	const stream = FS.createWriteStream(Path.join(__dirname, 'data/fakerdata.csv'))
+	const stream = FS.createWriteStream(Path.join(__dirname, 'fakerdata.csv'))
 	let generated = 0
 
 	const progressBar = new CLIProgress.SingleBar(
@@ -24,15 +24,17 @@ if (Cluster.isMaster) {
 		CLIProgress.Presets.shades_grey
 	)
 
-	const csvTransformer = new Transform({}, { highWaterMark: 16384, encoding: 'utf-8' })
+	const csvTransformer = new Transform({}, {
+		highWaterMark: 16384,
+		encoding: 'utf-8'
+	})
 
 	csvTransformer.pipe(stream)
 	progressBar.start(DATASET_SIZE, 0)
 
 	Cluster.on('message', (w, m) => {
-		csvTransformer.write(m)
 		generated++
-
+		csvTransformer.write(m)
 		progressBar.increment()
 
 		if (generated > DATASET_SIZE) {
@@ -41,7 +43,6 @@ if (Cluster.isMaster) {
 			}
 
 			stream.close()
-
 			process.exit()
 		}
 	})
@@ -51,9 +52,8 @@ if (Cluster.isMaster) {
 	}
 } else {
 	for (let i = 0; i < TARGET_PER_WORKER; i++) {
-		const { name, username, email, phone, website } = Faker.helpers.createCard()
-
-		process.send(JSON.stringify({ name, username, email, phone, website }))
+		const {name, username, email, phone, website} = Faker.helpers.createCard()
+		process.send(JSON.stringify({name, username, email, phone, website}))
 	}
 
 	process.exit()
