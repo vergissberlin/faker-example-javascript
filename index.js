d5596da7b0459a2c0a5937dfd3de280da6a5cf6b
Andre Lademann <andre.lademann@netresearch.de>
2021-07-09 22:44:55 +0200

    Use workerpool library

--- index.js
+++ index.js
@@ -1,76 +1,77 @@
-const Cluster = require('cluster')
-const Faker = require('faker')
-const FS = require('fs')
-const Path = require('path')
-const OS = require('os')
+const workerpool = require('workerpool');
 const CLIProgress = require('cli-progress')
-const JSON2CSV = require('json2csv')
 
-/**
- * Workers threads
- *
- * @see https://nodejs.org/api/worker_threads.html
- */
-if (Cluster.isMaster) {
-	/**
-	 * Configure to your needs
-	 */
-	const THREADS = !isNaN(parseInt(process.env.THREADS)) ? parseInt(process.env.THREADS) : OS.cpus().length
-	const TARGET = !isNaN(parseInt(process.env.TARGET)) ? parseInt(process.env.TARGET) : 10000000
-	const TARGET_PER_THREAD = Math.ceil(TARGET / THREADS)
+// Configuration
+const LINES_PER_FILE = 1000
+const TIME_START = process.hrtime();
+let FAKE_DATA_TARGET_QUANTATY = 500000
+let fileTargetQuantity = Math.ceil(FAKE_DATA_TARGET_QUANTATY / LINES_PER_FILE)
+let fileIndex = 0
+let fileQuantaty = 0
 
-	console.log(`🔥 Generating ${TARGET} fake users on ${THREADS} threads, with every thread generating ${TARGET_PER_THREAD} users.`)
+// Intro
+console.log(`\n🔥\tGenerating ${fileTargetQuantity} files with ${LINES_PER_FILE} fake users in each file.`)
+console.log(`\tIn total, you get ${FAKE_DATA_TARGET_QUANTATY} fake users.`)
+console.log(`\tTo generate them it uses all of your ${workerpool.cpus} CPUs.\n`)
 
-	let generated = 0
+// Debugging
+/*
+const {totalWorkers, busyWorkers, idleWorkers, pendingTasks, activeTasks} = pool.stats()
+console.log(`\nTotal workers\t:`, totalWorkers)
+console.log(`Idle workers\t:`, idleWorkers)
+console.log(`Pending tasks\t:`, pendingTasks)
+console.log(`Active tasks\t:`, activeTasks)
+console.log(`CPUs available\t: ${workerpool.cpus}\n`)
+*/
 
-	const progressBar = new CLIProgress.SingleBar(
-		{
-			format: '🤖 Generating Datasets: [' + '{bar}' + '] {percentage}% | {value}/{total} Datasets',
-			hideCursor: true,
-			clearOnComplete: true,
-			stopOnComplete: true
-		},
-		CLIProgress.Presets.shades_grey
-	)
+// For better visualization,start an progress bar
+const progressBar = new CLIProgress.SingleBar(
+	{
+		format: '🤖 Generating datasets: [' + '{bar}' + '] {percentage}% | {value}/{total} files.',
+		hideCursor: true,
+		clearOnComplete: true,
+		stopOnComplete: true
+	},
+	CLIProgress.Presets.shades_grey
+)
 
-	progressBar.start(TARGET, 0)
+// Create a worker pool using an external worker script
+const pool = workerpool.pool(__dirname + '/worker.js', {
+	workerType: 'auto'
+});
 
-	Cluster.on('message', () => {
-		generated++
-		progressBar.increment()
+// Start progress bar
+progressBar.start(fileTargetQuantity, 0)
 
-		if (generated > TARGET) {
-			for (const id in Cluster.workers) {
-				Cluster.workers[id].send('stop')
-			}
-		}
-	})
+// Run registered functions on the worker via exec
+function spawnWorker(index) {
+	if (fileIndex < fileTargetQuantity) {
+		fileIndex++
 
-	for (let i = 0; i < THREADS; i++) {
-		Cluster.fork({FILE: Path.join(__dirname, `data/fakerdata_${i}.csv`), TARGET_PER_THREAD})
-	}
-} else {
-	process.on('message', (m) => {
-		if (m === 'stop') {
-			process.exit()
-		}
-	})
-
-	FS.writeFileSync(process.env.FILE, 'name,username,email,phone,website')
-
-	for (let i = 0; i <= parseInt(process.env.TARGET_PER_THREAD); i++) {
-		/**
-		 * @see https://github.com/marak/Faker.js
-		 */
-		const {name, username, email, phone, website} = Faker.helpers.createCard()
-
-		FS.appendFileSync(
-			process.env.FILE,
-			'\n' + JSON2CSV.parse([{name, username, email, phone, website}], {
-				header: false
+		pool.exec('fakeIt', [index, LINES_PER_FILE])
+			.then(function (result) {
+				fileQuantaty++
+				progressBar.increment()
+				// Spawn new workers as soon a slot is available.
+				spawnWorker(fileIndex)
 			})
-		)
-
-		process.send(i)
+			.catch(function (err) {
+				console.error(err)
+			})
+			.then(function () {
+				// Terminate the workers pool when all workers have finised their tasks
+				if (fileQuantaty === fileTargetQuantity) {
+					const TIME_STOP = process.hrtime(TIME_START)
+					console.log(`Created fake users\t:`, FAKE_DATA_TARGET_QUANTATY)
+					console.log(`Time taken to execute\t: ${(TIME_STOP[0] * 1e9 + TIME_STOP[1]) / 1e9} seconds`)
+					pool.terminate();
+				}
+			})
 	}
 }
+
+// Spawn the first workers. One for each available core.
+for (let threadIndex = 0; threadIndex < workerpool.cpus; threadIndex++) {
+	spawnWorker(threadIndex)
+}
+
---
+++ worker.js
@@ -0,0 +1,31 @@
+// file myWorker.js
+const workerpool = require('workerpool');
+const faker = require('faker')
+const JSON2CSV = require('json2csv')
+const Path = require('path')
+
+function fakeIt(fileIndex, quantity) {
+  return new Promise(function (resolve, reject) {
+    let fakeDataArray = []
+    for (let i = 0; i < quantity; i++) {
+      const {name, username, email, phone, website} = faker.helpers.createCard()
+      fakeDataArray.push({name, username, email, phone, website})
+    }
+
+    const fakeData = JSON2CSV.parse(fakeDataArray)
+    let fs = require('fs')
+    fs.writeFile(Path.join(__dirname,`data/userdata-${fileIndex}.csv`), fakeData, function (err) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(fileIndex)
+      }
+    })
+  })
+}
+
+// create a worker and register functions
+workerpool.worker({
+  fakeIt: fakeIt
+});
+
